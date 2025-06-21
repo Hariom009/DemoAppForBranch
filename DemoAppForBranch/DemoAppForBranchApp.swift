10e60aef00c21b121ca360bde5e30efa0560f652
Hariom <hariomconnects03@gmail.com>
2025-06-21 23:19:55 +0530

    Initial Commit

---
+++ DemoAppForBranch/DemoAppForBranchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppForBranchApp.swift
+//  DemoAppForBranch
+//
+//  Created by Hari's Mac on 21.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppForBranchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
